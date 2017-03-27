2bfdf8ba7a47968bd4604cf8ca5473b1d6de9f36
Unknown <xiaow6@uci.edu>
2017-03-27 14:35:32 +0800

    PointerOnC: Chapter 1 problem 4
    strcpy is missing first char for output string.

---
+++ PointerOnC/Chapter_1/problem4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*it should be 1001 because '\0'*/
+#define MAX_LEN 1001
+int main(void) {
+	char input[MAX_LEN];
+	char output[MAX_LEN];
+	int len;
+	/*为了对应检查，将其改为 -1*/
+	int len_max = -1;
+	char ch;
+	while (gets (input) != NULL) {
+		len = strlen(input);
+		if (len >= len_max) {
+			len_max = len;
+			strcpy(output, input);   
+			/*string 赋值的方法*/
+		}
+		printf("%s\n", output);
+		if ((ch = getchar()) == '\n') break;
+	}
+	/*做一道检查，不直接打印,input有能为空*/
+	if (len_max >= 0)
+		printf("%s\n", output);
+	return EXIT_SUCCESS;
+}
