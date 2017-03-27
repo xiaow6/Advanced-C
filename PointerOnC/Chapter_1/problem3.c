14bd46ac69b9732ebe31c8fd33ac8ab02cd214f6
Unknown <xiaow6@uci.edu>
2017-03-27 11:12:03 +0800

    PointerOnC: Chapter 1,problem 3

--- PointerOnC/Chapter_1/problem3.c
+++ PointerOnC/Chapter_1/problem3.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+	signed char checksum = -1;
+	char ch;
+	while ((ch = getchar()) != EOF) {
+		checksum += ch;
+		putchar(ch);
+		if (ch == '\n') break;
+	}
+	if (checksum > 127) 
+		checksum = 127;
+	else if (checksum < -128) 
+		checksum = -128;
+	printf("checksum = %d\n", checksum);
+}
